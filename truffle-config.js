4db961abb0cd0bbc829beffa3d0920a282c8a54e
didi <git@d10r.net>
2022-10-03 17:36:47 +0200

    remove development network, blocks tests

--- truffle-config.js
+++ truffle-config.js
@@ -3,12 +3,6 @@ const HDWalletProvider = require("@truffle/hdwallet-provider")
 
 module.exports = {
 	networks: {
-		development: {
-			host: "127.0.0.1",
-			port: 9545,
-			network_id: "*"
-		},
-
 		// goerli testnet
 		goerli: {
 			provider: () =>
