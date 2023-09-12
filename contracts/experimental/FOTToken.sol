21488c218a9a68fefc6788ccc1e47b3a2c6bc9ea
didi <git@d10r.net>
2023-09-12 15:12:27 +0200

    possible implementation of a FOTToken

---
+++ contracts/experimental/FOTToken.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: AGPLv3
+pragma solidity ^0.8.0;
+
+import {SuperTokenStorage} from "../base/SuperTokenStorage.sol";
+import {UUPSProxy} from "../base/UUPSProxy.sol";
+import {
+    ISuperToken,
+    ISuperTokenFactory,
+    IERC20
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title FOT (Fee on Transfer) Token
+/// trivial implementation using a fixed fee amount per tx, added to the actual transfer amount.
+/// @notice SuperTokenStorage MUST be the first inherited contract, otherwise the storage layout may get corrupted
+contract FOTTokenProxy is SuperTokenStorage, UUPSProxy, Ownable {
+    uint256 public feePerTx;
+    address public feeRecipient;
+
+    constructor(uint256 _feePerTx, address _feeRecipient) {
+        feePerTx = _feePerTx;
+        feeRecipient = _feeRecipient;
+    }
+
+    function setFeeConfig(uint256 _feePerTx, address _feeRecipient) external onlyOwner {
+        feePerTx = _feePerTx;
+        feeRecipient = _feeRecipient;
+    }
+
+    function initialize(
+        ISuperTokenFactory factory,
+        string memory name,
+        string memory symbol
+    ) external {
+        ISuperTokenFactory(factory).initializeCustomSuperToken(address(this));
+		ISuperToken(address(this)).initialize(IERC20(address(0)), 18, name, symbol);
+    }
+
+    // ERC20 functions we want to intercept and add custom behavior to
+
+    function transferFrom(address holder, address recipient, uint256 amount)
+        public returns (bool)
+    {
+        // get the fee
+        ISuperToken(address(this)).selfTransferFrom(holder, msg.sender, feeRecipient, feePerTx);
+        // (external) call into the implementation contract.
+
+        // do the actual tx
+        ISuperToken(address(this)).selfTransferFrom(holder, msg.sender, recipient, amount);
+        return true; // returns true if it didn't revert
+    }
+
+    function transfer(address recipient, uint256 amount)
+        public returns (bool)
+    {
+        return transferFrom(msg.sender, recipient, amount);
+    }
+}
