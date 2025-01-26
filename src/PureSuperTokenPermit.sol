a27aff64cd8eda52f3d49d97033ed29ae64cee5f
didi <git@d10r.net>
2025-01-26 12:46:26 +0100

    forge fmt formatted

--- src/PureSuperTokenPermit.sol
+++ src/PureSuperTokenPermit.sol
@@ -1,20 +1,28 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.26;
 
-import { IERC20Permit } from "@openzeppelin-v5/contracts/token/ERC20/extensions/IERC20Permit.sol";
-import { ECDSA } from "@openzeppelin-v5/contracts/utils/cryptography/ECDSA.sol";
-import { EIP712 } from "@openzeppelin-v5/contracts/utils/cryptography/EIP712.sol";
-import { Nonces } from "@openzeppelin-v5/contracts/utils/Nonces.sol";
+import {IERC20Permit} from "@openzeppelin-v5/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {ECDSA} from "@openzeppelin-v5/contracts/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin-v5/contracts/utils/cryptography/EIP712.sol";
+import {Nonces} from "@openzeppelin-v5/contracts/utils/Nonces.sol";
 
-import { CustomSuperTokenBase } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/CustomSuperTokenBase.sol";
-import { UUPSProxy } from "@superfluid-finance/ethereum-contracts/contracts/upgradability/UUPSProxy.sol";
-import { ISuperToken, ISuperTokenFactory, IERC20 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
-import { ISuperToken, ISuperTokenFactory, IERC20 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {CustomSuperTokenBase} from
+    "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/CustomSuperTokenBase.sol";
+import {UUPSProxy} from "@superfluid-finance/ethereum-contracts/contracts/upgradability/UUPSProxy.sol";
+import {
+    ISuperToken,
+    ISuperTokenFactory,
+    IERC20
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {
+    ISuperToken,
+    ISuperTokenFactory,
+    IERC20
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 
 /// @title The Proxy contract for a Pure SuperToken with permit and preminted initial supply.
 contract PureSuperTokenPermitProxy is CustomSuperTokenBase, UUPSProxy, IERC20Permit, EIP712, Nonces {
-
-	bytes32 private constant PERMIT_TYPEHASH =
+    bytes32 private constant PERMIT_TYPEHASH =
         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
     /// @dev Permit deadline has expired.
@@ -23,47 +31,37 @@ contract PureSuperTokenPermitProxy is CustomSuperTokenBase, UUPSProxy, IERC20Per
     /// @dev Mismatched signature.
     error ERC2612InvalidSigner(address signer, address owner);
 
-	constructor(string memory name) EIP712(name, "1") {}
+    constructor(string memory name) EIP712(name, "1") {}
 
-	// This shall be invoked exactly once after deployment, needed for the token contract to become operational.
-	function initialize(
-		ISuperTokenFactory factory,
-		string memory name,
-		string memory symbol,
-		address receiver,
-		uint256 initialSupply
-	) external {
-		// This call to the factory invokes `UUPSProxy.initialize`, which connects the proxy to the canonical SuperToken implementation.
-		// It also emits an event which facilitates discovery of this token.
-		ISuperTokenFactory(factory).initializeCustomSuperToken(address(this));
+    // This shall be invoked exactly once after deployment, needed for the token contract to become operational.
+    function initialize(
+        ISuperTokenFactory factory,
+        string memory name,
+        string memory symbol,
+        address receiver,
+        uint256 initialSupply
+    ) external {
+        // This call to the factory invokes `UUPSProxy.initialize`, which connects the proxy to the canonical SuperToken implementation.
+        // It also emits an event which facilitates discovery of this token.
+        ISuperTokenFactory(factory).initializeCustomSuperToken(address(this));
 
-		// This initializes the token storage and sets the `initialized` flag of OpenZeppelin Initializable.
-		// This makes sure that it will revert if invoked more than once.
-		ISuperToken(address(this)).initialize(
-			IERC20(address(0)),
-			18,
-			name,
-			symbol
-		);
+        // This initializes the token storage and sets the `initialized` flag of OpenZeppelin Initializable.
+        // This makes sure that it will revert if invoked more than once.
+        ISuperToken(address(this)).initialize(IERC20(address(0)), 18, name, symbol);
 
-		// This mints the specified initial supply to the specified receiver.
-		ISuperToken(address(this)).selfMint(receiver, initialSupply, "");
-	}
+        // This mints the specified initial supply to the specified receiver.
+        ISuperToken(address(this)).selfMint(receiver, initialSupply, "");
+    }
 
-	// ============================ IERC20Permit ============================
+    // ============================ IERC20Permit ============================
 
-	/**
+    /**
      * @inheritdoc IERC20Permit
      */
-    function permit(
-        address owner,
-        address spender,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) public virtual {
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        public
+        virtual
+    {
         if (block.timestamp > deadline) {
             revert ERC2612ExpiredSignature(deadline);
         }
@@ -77,7 +75,7 @@ contract PureSuperTokenPermitProxy is CustomSuperTokenBase, UUPSProxy, IERC20Per
             revert ERC2612InvalidSigner(signer, owner);
         }
 
-		ISuperToken(address(this)).selfApproveFor(owner, spender, value);
+        ISuperToken(address(this)).selfApproveFor(owner, spender, value);
     }
 
     /**
--- test/PureSuperTokenPermit.t.sol
+++ test/PureSuperTokenPermit.t.sol
@@ -1,126 +1,104 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import { Test } from "forge-std/Test.sol";
-import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
-import { SuperfluidFrameworkDeployer } from "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.t.sol";
-import { ERC1820RegistryCompiled } from "@superfluid-finance/ethereum-contracts/contracts/libs/ERC1820RegistryCompiled.sol";
-import { PureSuperTokenPermitProxy, IPureSuperTokenPermit } from "../src/PureSuperTokenPermit.sol";
+import {Test} from "forge-std/Test.sol";
+import {ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {SuperfluidFrameworkDeployer} from
+    "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.t.sol";
+import {ERC1820RegistryCompiled} from
+    "@superfluid-finance/ethereum-contracts/contracts/libs/ERC1820RegistryCompiled.sol";
+import {PureSuperTokenPermitProxy, IPureSuperTokenPermit} from "../src/PureSuperTokenPermit.sol";
 
 contract PureSuperTokenPermitTest is Test {
-	string constant internal _NAME = "TestToken";
-	address constant internal _OWNER = address(0x1);
-	uint256 constant internal _PERMIT_SIGNER_PK = 0xA11CE;
-	address internal _permitSigner;
-	IPureSuperTokenPermit internal _superTokenPermit;
-	SuperfluidFrameworkDeployer.Framework internal _sf;
+    string internal constant _NAME = "TestToken";
+    address internal constant _OWNER = address(0x1);
+    uint256 internal constant _PERMIT_SIGNER_PK = 0xA11CE;
+    address internal _permitSigner;
+    IPureSuperTokenPermit internal _superTokenPermit;
+    SuperfluidFrameworkDeployer.Framework internal _sf;
 
-	function setUp() public {
-		vm.etch(ERC1820RegistryCompiled.at, ERC1820RegistryCompiled.bin);
-		SuperfluidFrameworkDeployer sfDeployer = new SuperfluidFrameworkDeployer();
-		sfDeployer.deployTestFramework();
-		_sf = sfDeployer.getFramework();
+    function setUp() public {
+        vm.etch(ERC1820RegistryCompiled.at, ERC1820RegistryCompiled.bin);
+        SuperfluidFrameworkDeployer sfDeployer = new SuperfluidFrameworkDeployer();
+        sfDeployer.deployTestFramework();
+        _sf = sfDeployer.getFramework();
 
-		PureSuperTokenPermitProxy superTokenPermitProxy = new PureSuperTokenPermitProxy(_NAME);
-		superTokenPermitProxy.initialize(_sf.superTokenFactory, _NAME, "TST", _OWNER, 1000);
-		_superTokenPermit = IPureSuperTokenPermit(address(superTokenPermitProxy));
+        PureSuperTokenPermitProxy superTokenPermitProxy = new PureSuperTokenPermitProxy(_NAME);
+        superTokenPermitProxy.initialize(_sf.superTokenFactory, _NAME, "TST", _OWNER, 1000);
+        _superTokenPermit = IPureSuperTokenPermit(address(superTokenPermitProxy));
 
-		// Generate signer address from private key
-		_permitSigner = vm.addr(_PERMIT_SIGNER_PK);
+        // Generate signer address from private key
+        _permitSigner = vm.addr(_PERMIT_SIGNER_PK);
 
-		// Fund the signer with some tokens
-		vm.prank(_OWNER);
-		_superTokenPermit.transfer(_permitSigner, 500);
-	}
+        // Fund the signer with some tokens
+        vm.prank(_OWNER);
+        _superTokenPermit.transfer(_permitSigner, 500);
+    }
 
-	function testPermit() public {
-		// Test parameters
-		address spender = address(0x2);
-		uint256 value = 100;
-		uint256 deadline = block.timestamp + 1 hours;
-		
-		// Get the current nonce for signer
-		uint256 nonce = _superTokenPermit.nonces(_permitSigner);
+    function testPermit() public {
+        // Test parameters
+        address spender = address(0x2);
+        uint256 value = 100;
+        uint256 deadline = block.timestamp + 1 hours;
 
-		assertEq(_superTokenPermit.allowance(_permitSigner, spender), 0, "Allowance should be 0");
+        // Get the current nonce for signer
+        uint256 nonce = _superTokenPermit.nonces(_permitSigner);
 
-		// Create permit digest
-		bytes32 digest = _createPermitDigest(
-			_permitSigner,
-			spender,
-			value,
-			nonce,
-			deadline
-		);
-		
-		// Create signature
-		(uint8 v, bytes32 r, bytes32 s) = vm.sign(_PERMIT_SIGNER_PK, digest);
-		
-		// Execute permit as a different address
-		vm.startPrank(address(0x3));
-		
-		// expect revert if spender doesn't match
-		vm.expectRevert();
-		_superTokenPermit.permit(_permitSigner, address(0xfefe), value, deadline, v, r, s);
+        assertEq(_superTokenPermit.allowance(_permitSigner, spender), 0, "Allowance should be 0");
 
-		// expect revert if value doesn't match
-		vm.expectRevert();
-		_superTokenPermit.permit(_permitSigner, spender, value + 1, deadline, v, r, s);
+        // Create permit digest
+        bytes32 digest = _createPermitDigest(_permitSigner, spender, value, nonce, deadline);
 
-		// expect revert if signature is invalid
-		vm.expectRevert();
-		_superTokenPermit.permit(_permitSigner, spender, value, deadline, v + 1, r, s);
+        // Create signature
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_PERMIT_SIGNER_PK, digest);
 
-		uint256 prevBlockTS = block.timestamp;
-		vm.warp(block.timestamp + deadline + 1);
-		// expect revert if deadline is in the past
-		vm.expectRevert();
-		_superTokenPermit.permit(_permitSigner, spender, value, deadline, v, r, s);
-		
-		vm.warp(prevBlockTS);
+        // Execute permit as a different address
+        vm.startPrank(address(0x3));
 
-		// Now test with correct parameters - should succeed
-		_superTokenPermit.permit(_permitSigner, spender, value, deadline, v, r, s);
+        // expect revert if spender doesn't match
+        vm.expectRevert();
+        _superTokenPermit.permit(_permitSigner, address(0xfefe), value, deadline, v, r, s);
 
-		vm.stopPrank();
-		
-		// Verify results
-		assertEq(_superTokenPermit.nonces(_permitSigner), 1, "Nonce should be incremented");
-		assertEq(_superTokenPermit.allowance(_permitSigner, spender), value, "Allowance should be set");
-	}
+        // expect revert if value doesn't match
+        vm.expectRevert();
+        _superTokenPermit.permit(_permitSigner, spender, value + 1, deadline, v, r, s);
 
-	// ============================ Internal Functions ============================
+        // expect revert if signature is invalid
+        vm.expectRevert();
+        _superTokenPermit.permit(_permitSigner, spender, value, deadline, v + 1, r, s);
 
-	function _createPermitDigest(
-		address owner,
-		address spender,
-		uint256 value,
-		uint256 nonce,
-		uint256 deadline
-	) internal view returns (bytes32) {
-		bytes32 PERMIT_TYPEHASH = keccak256(
-			"Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-		);
-		
-		bytes32 structHash = keccak256(
-			abi.encode(
-				PERMIT_TYPEHASH,
-				owner,
-				spender,
-				value,
-				nonce,
-				deadline
-			)
-		);
-		
-		bytes32 DOMAIN_SEPARATOR = _superTokenPermit.DOMAIN_SEPARATOR();
-		
-		return keccak256(
-			abi.encodePacked(
-				"\x19\x01",
-				DOMAIN_SEPARATOR,
-				structHash
-			)
-		);
-	}
+        uint256 prevBlockTS = block.timestamp;
+        vm.warp(block.timestamp + deadline + 1);
+        // expect revert if deadline is in the past
+        vm.expectRevert();
+        _superTokenPermit.permit(_permitSigner, spender, value, deadline, v, r, s);
+
+        vm.warp(prevBlockTS);
+
+        // Now test with correct parameters - should succeed
+        _superTokenPermit.permit(_permitSigner, spender, value, deadline, v, r, s);
+
+        vm.stopPrank();
+
+        // Verify results
+        assertEq(_superTokenPermit.nonces(_permitSigner), 1, "Nonce should be incremented");
+        assertEq(_superTokenPermit.allowance(_permitSigner, spender), value, "Allowance should be set");
+    }
+
+    // ============================ Internal Functions ============================
+
+    function _createPermitDigest(address owner, address spender, uint256 value, uint256 nonce, uint256 deadline)
+        internal
+        view
+        returns (bytes32)
+    {
+        bytes32 PERMIT_TYPEHASH =
+            keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+        bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonce, deadline));
+
+        bytes32 DOMAIN_SEPARATOR = _superTokenPermit.DOMAIN_SEPARATOR();
+
+        return keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, structHash));
+    }
 }
