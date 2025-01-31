a273ce2a03137ee78e77993bd0a58c2a6e40a829
didi <git@d10r.net>
2025-01-31 21:22:16 +0100

    forge fmt, added flow test

--- src/PureSuperToken.sol
+++ src/PureSuperToken.sol
@@ -4,38 +4,38 @@ pragma solidity ^0.8.26;
 // This file contains everything we need for a minimal Pure SuperToken.
 
 // This abstract contract provides storage padding for the proxy
-import { CustomSuperTokenBase } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/CustomSuperTokenBase.sol";
+import {CustomSuperTokenBase} from
+    "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/CustomSuperTokenBase.sol";
 // Implementation of UUPSProxy (see https://eips.ethereum.org/EIPS/eip-1822)
-import { UUPSProxy } from "@superfluid-finance/ethereum-contracts/contracts/upgradability/UUPSProxy.sol";
+import {UUPSProxy} from "@superfluid-finance/ethereum-contracts/contracts/upgradability/UUPSProxy.sol";
 // Superfluid framework interfaces we need
-import { ISuperToken, ISuperTokenFactory, IERC20 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {
+    ISuperToken,
+    ISuperTokenFactory,
+    IERC20
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 
 /// @title The Proxy contract for a Pure SuperToken with preminted initial supply.
 contract PureSuperTokenProxy is CustomSuperTokenBase, UUPSProxy {
-	// This shall be invoked exactly once after deployment, needed for the token contract to become operational.
-	function initialize(
-		ISuperTokenFactory factory,
-		string memory name,
-		string memory symbol,
-		address receiver,
-		uint256 initialSupply
-	) external {
-		// This call to the factory invokes `UUPSProxy.initialize`, which connects the proxy to the canonical SuperToken implementation.
-		// It also emits an event which facilitates discovery of this token.
-		ISuperTokenFactory(factory).initializeCustomSuperToken(address(this));
+    // This shall be invoked exactly once after deployment, needed for the token contract to become operational.
+    function initialize(
+        ISuperTokenFactory factory,
+        string memory name,
+        string memory symbol,
+        address receiver,
+        uint256 initialSupply
+    ) external {
+        // This call to the factory invokes `UUPSProxy.initialize`, which connects the proxy to the canonical SuperToken implementation.
+        // It also emits an event which facilitates discovery of this token.
+        ISuperTokenFactory(factory).initializeCustomSuperToken(address(this));
 
-		// This initializes the token storage and sets the `initialized` flag of OpenZeppelin Initializable.
-		// This makes sure that it will revert if invoked more than once.
-		ISuperToken(address(this)).initialize(
-			IERC20(address(0)),
-			18,
-			name,
-			symbol
-		);
+        // This initializes the token storage and sets the `initialized` flag of OpenZeppelin Initializable.
+        // This makes sure that it will revert if invoked more than once.
+        ISuperToken(address(this)).initialize(IERC20(address(0)), 18, name, symbol);
 
-		// This mints the specified initial supply to the specified receiver.
-		ISuperToken(address(this)).selfMint(receiver, initialSupply, "");
-	}
+        // This mints the specified initial supply to the specified receiver.
+        ISuperToken(address(this)).selfMint(receiver, initialSupply, "");
+    }
 }
 
 // The token interface is just an alias of ISuperToken
--- test/PureSuperToken.t.sol
+++ test/PureSuperToken.t.sol
@@ -1,40 +1,64 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import { Test } from "forge-std/Test.sol";
-import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
-import { SuperfluidFrameworkDeployer } from "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.t.sol";
-import { ERC1820RegistryCompiled } from "@superfluid-finance/ethereum-contracts/contracts/libs/ERC1820RegistryCompiled.sol";
-import { PureSuperTokenProxy } from "../src/PureSuperToken.sol";
+import {Test} from "forge-std/Test.sol";
+import {ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {SuperfluidFrameworkDeployer} from
+    "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.t.sol";
+import {ERC1820RegistryCompiled} from
+    "@superfluid-finance/ethereum-contracts/contracts/libs/ERC1820RegistryCompiled.sol";
+import {SuperTokenV1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
+import {PureSuperTokenProxy, IPureSuperToken} from "../src/PureSuperToken.sol";
+
+using SuperTokenV1Library for IPureSuperToken;
 
 contract PureSuperTokenProxyTest is Test {
-	address constant internal _OWNER = address(0x1);
-	PureSuperTokenProxy internal _superTokenProxy;
-	SuperfluidFrameworkDeployer.Framework internal _sf;
+    address internal constant _OWNER = address(0x1);
+    uint256 internal constant _INITIAL_SUPPLY = 1000 ether;
+    address internal constant _ALICE = address(0x4242);
+    address internal constant _BOB = address(0x4243);
 
-	function setUp() public {
-		vm.etch(ERC1820RegistryCompiled.at, ERC1820RegistryCompiled.bin);
-		SuperfluidFrameworkDeployer sfDeployer = new SuperfluidFrameworkDeployer();
-		sfDeployer.deployTestFramework();
-		_sf = sfDeployer.getFramework();
-	}
+    SuperfluidFrameworkDeployer.Framework internal _sf;
+    IPureSuperToken internal _superToken;
 
-	function testDeploy() public {
-		_superTokenProxy = new PureSuperTokenProxy();
-		assert(address(_superTokenProxy) != address(0));
-	}
+    function setUp() public {
+        PureSuperTokenProxy _superTokenProxy;
+        vm.etch(ERC1820RegistryCompiled.at, ERC1820RegistryCompiled.bin);
+        SuperfluidFrameworkDeployer sfDeployer = new SuperfluidFrameworkDeployer();
+        sfDeployer.deployTestFramework();
+        _sf = sfDeployer.getFramework();
 
-	function testSuperTokenBalance() public {
-		_superTokenProxy = new PureSuperTokenProxy();
-		_superTokenProxy.initialize(
-			_sf.superTokenFactory,
-			"TestToken",
-			"TST",
-			_OWNER,
-			1000
-		);
-		ISuperToken superToken = ISuperToken(address(_superTokenProxy));
-		uint balance = superToken.balanceOf(_OWNER);
-		assert(balance == 1000);
-	}
+        // deploy the proxy
+        PureSuperTokenProxy superTokenProxy = _superTokenProxy = new PureSuperTokenProxy();
+        _superTokenProxy.initialize(_sf.superTokenFactory, "TestToken", "TST", _ALICE, _INITIAL_SUPPLY);
+        _superToken = IPureSuperToken(address(superTokenProxy));
+    }
+
+    function testInitialMintBalance() public {
+        assert(_superToken.balanceOf(_ALICE) == _INITIAL_SUPPLY);
+    }
+
+    function testFlow() public {
+        int96 flowRate = 1e12;
+        uint256 duration = 3600;
+
+        uint256 aliceInitialBalance = _superToken.balanceOf(_ALICE);
+        assertEq(_superToken.balanceOf(_BOB), 0, "Bob should start with balance 0");
+
+        vm.startPrank(_ALICE);
+        _superToken.createFlow(_BOB, flowRate);
+        vm.stopPrank();
+
+        vm.warp(block.timestamp + duration);
+
+        uint256 flowAmount = uint96(flowRate) * duration;
+        assertEq(_superToken.balanceOf(_BOB), flowAmount, "Bob unexpected balance");
+
+        vm.startPrank(_ALICE);
+        _superToken.deleteFlow(_ALICE, _BOB);
+        vm.stopPrank();
+
+        assertEq(_superToken.balanceOf(_BOB), flowAmount, "Bob unexpected balance");
+        assertEq(_superToken.balanceOf(_ALICE), aliceInitialBalance - flowAmount, "Alice unexpected balance");
+    }
 }
